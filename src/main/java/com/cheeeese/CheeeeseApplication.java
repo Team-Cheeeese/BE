a6b702d0e8acb43ced69d9db18e5b3a4096a710f
zyovn <yui2507@naver.com>
2025-10-02 00:34:31 +0900

    #1 chore: 프로젝트 초기 설정

---
+++ src/main/java/com/cheeeese/CheeeeseApplication.java
@@ -0,0 +1,13 @@
+package com.cheeeese;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheeeeseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheeeeseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cheeeese/CheeeeseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheeeese;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheeeeseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
