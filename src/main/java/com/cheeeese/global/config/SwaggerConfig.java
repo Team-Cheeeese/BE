a4efc4138bdb41a0e775e0dca014fdbc96c04aeb
zyovn <yui2507@naver.com>
2025-10-02 12:00:15 +0900

    #1 chore: swaggerConfig에 configuration 어노테이션 추가

--- src/main/java/com/cheeeese/global/config/SwaggerConfig.java
+++ src/main/java/com/cheeeese/global/config/SwaggerConfig.java
@@ -3,7 +3,9 @@ package com.cheeeese.global.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @OpenAPIDefinition(
         info = @Info(
                 title = "Cheeeese API 명세서",
