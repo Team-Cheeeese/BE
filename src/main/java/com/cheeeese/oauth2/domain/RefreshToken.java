14847cdd8fef388cf8a49af21746b7041f34d8ab
zyovn <yui2507@naver.com>
2025-10-05 18:32:49 +0900

    #3 feat: refreshToken 관련 로직 구현

---
+++ src/main/java/com/cheeeese/oauth2/domain/RefreshToken.java
@@ -0,0 +1,30 @@
+package com.cheeeese.oauth2.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+
+@Getter
+@NoArgsConstructor
+@RedisHash(value = "refreshToken")
+public class RefreshToken {
+
+    @Id
+    private Long userId;
+
+    private String refreshToken;
+
+    @TimeToLive
+    private Long expiration = 604800L;
+
+    public RefreshToken(Long userId, String refreshToken) {
+        this.userId = userId;
+        this.refreshToken = refreshToken;
+    }
+
+    public void updateRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+}
