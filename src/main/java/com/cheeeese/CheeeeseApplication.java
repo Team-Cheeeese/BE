3db1f068f505671289690641b3000805e745ec0e
zyovn <yui2507@naver.com>
2025-10-06 00:45:27 +0900

    #3 feat: currentUser 어노테이션 구현 및 redisRepository 위치 명시

--- src/main/java/com/cheeeese/CheeeeseApplication.java
+++ src/main/java/com/cheeeese/CheeeeseApplication.java
@@ -3,9 +3,11 @@ package com.cheeeese;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableRedisRepositories(basePackages = "com.cheeeese.oauth2.infrastructure.persistence")
 public class CheeeeseApplication {
 
 	public static void main(String[] args) {
