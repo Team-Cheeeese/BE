3cdbea3a194324f3530f0e75de4f86f81e6e5b2e
다현 <123882512+dahyun24@users.noreply.github.com>
2025-10-14 02:11:33 +0900

    #12 feat: useralbum 엔티티 생성 및 role eunm 생성

---
+++ src/main/java/com/cheeeese/album/domain/UserAlbum.java
@@ -0,0 +1,39 @@
+package com.cheeeese.album.domain;
+
+import com.cheeeese.global.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "user_album")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserAlbum extends BaseEntity {
+
+    @Id
+    @Column(name = "user_album_id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // TODO: 추후 필요시 ManyToOne, JoinColumn 넣을 예정
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    // TODO: 추후 필요시 ManyToOne, JoinColumn 넣을 예정
+    @Column(name = "album_id", nullable = false)
+    private Long albumId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private UserAlbumRole role;
+
+    @Builder
+    public UserAlbum(Long userId, Long albumId, UserAlbumRole role) {
+        this.userId = userId;
+        this.albumId = albumId;
+        this.role = role;
+    }
+}
---
+++ src/main/java/com/cheeeese/album/domain/UserAlbumRole.java
@@ -0,0 +1,7 @@
+package com.cheeeese.album.domain;
+
+public enum UserAlbumRole {
+    HOST,
+    GUEST,
+    BLACK
+}
