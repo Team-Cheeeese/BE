ea6fb619f65d391ec860882536784890aaaf3a18
다현 <123882512+dahyun24@users.noreply.github.com>
2025-10-14 13:43:44 +0900

    #12 feat: photoRepository 구현

---
+++ src/main/java/com/cheeeese/photo/infrastructure/persistence/PhotoRepository.java
@@ -0,0 +1,13 @@
+package com.cheeeese.photo.infrastructure.persistence;
+
+import com.cheeeese.photo.domain.Photo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PhotoRepository extends JpaRepository<Photo, Long> {
+
+    long countByAlbumIdAndIsDeletedFalse(Long albumId);
+
+    List<Photo> findTop9ByAlbumIdAndIsDeletedFalseOrderByCreatedAtDesc(Long albumId);
+}
