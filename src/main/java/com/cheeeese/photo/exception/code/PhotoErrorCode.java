2acfc949b03af40c8dd76dfede117d3b570131f8
다현 <123882512+dahyun24@users.noreply.github.com>
2025-10-24 13:45:50 +0900

    #16 remove: 사용하지 않는 에러 코드 제거

--- src/main/java/com/cheeeese/photo/exception/code/PhotoErrorCode.java
+++ src/main/java/com/cheeeese/photo/exception/code/PhotoErrorCode.java
@@ -9,8 +9,6 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum PhotoErrorCode implements BaseCode {
 
-    PHOTO_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않거나 유효하지 않은 사진 정보입니다."),
-
     PHOTO_MAX_COUNT_EXCEEDED(HttpStatus.BAD_REQUEST, "앨범의 최대 사진 개수를 초과합니다."),
     PHOTO_FILE_SIZE_EXCEEDED(HttpStatus.BAD_REQUEST, "파일 크기는 6MB를 초과할 수 없습니다."),
     PHOTO_INVALID_CONTENT_TYPE(HttpStatus.BAD_REQUEST, "유효한 이미지 파일 형식이 아닙니다."),
