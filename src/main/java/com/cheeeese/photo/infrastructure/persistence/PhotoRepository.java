f37af7fa867bd93874c9110271ed1443180fd1a4
다현 <123882512+dahyun24@users.noreply.github.com>
2025-11-05 17:08:22 +0900

    #39 feat: 띱을 많이 받은 4개의 사진 조회

--- src/main/java/com/cheeeese/photo/infrastructure/persistence/PhotoRepository.java
+++ src/main/java/com/cheeeese/photo/infrastructure/persistence/PhotoRepository.java
@@ -46,4 +46,17 @@ public interface PhotoRepository extends JpaRepository<Photo, Long> {
     @Query("update Photo p set p.status = :newStatus, p.thumbnailUrl = :thumbnailUrl " +
             "where p.id = :photoId and p.status = :expectedStatus")
     int updateStatusAndUrl(Long photoId, PhotoStatus expectedStatus, PhotoStatus newStatus, String thumbnailUrl);
+
+    @Query("""
+    SELECT p.id
+    FROM Photo p
+    WHERE p.album.id = :albumId
+    AND p.isDeleted = FALSE
+    AND p.status = com.cheeeese.photo.domain.PhotoStatus.COMPLETED
+    ORDER BY p.likesCnt DESC, p.createdAt DESC
+""")
+    List<Long> findTop4CompletedPhotoIdsByLikes(
+            @Param("albumId") Long albumId,
+            Pageable pageable
+    );
 }
