4a194080ad28d146c547a6bf6448a086c8be28c3
다현 <123882512+dahyun24@users.noreply.github.com>
2025-11-11 02:41:40 +0900

    #39 feat: 스케줄러 구현

--- src/main/java/com/cheeeese/CheeeeseApplication.java
+++ src/main/java/com/cheeeese/CheeeeseApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableRedisRepositories(basePackages = "com.cheeeese.auth.infrastructure.persistence")
+@EnableScheduling
 public class CheeeeseApplication {
 
 	public static void main(String[] args) {
