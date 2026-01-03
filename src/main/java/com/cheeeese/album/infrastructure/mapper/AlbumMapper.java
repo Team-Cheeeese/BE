521073d4395e78fdeb112a24f2254ecc2623da3d
zyovn <yui2507@naver.com>
2026-01-03 20:07:15 +0900

    #106 feat: 앨범 사진 제한 2000장으로 수정

--- src/main/java/com/cheeeese/album/infrastructure/mapper/AlbumMapper.java
+++ src/main/java/com/cheeeese/album/infrastructure/mapper/AlbumMapper.java
@@ -34,7 +34,7 @@ public class AlbumMapper {
                 .participant(participant)
                 .currentParticipant(1)
                 .eventDate(eventDate)
-                .maxPhotoCount(300)
+                .maxPhotoCount(2000)
                 .currentPhotoCount(0)
                 .isInfoAvailable(isInfoAvailable)
                 .expiredAt(expiredAt)
