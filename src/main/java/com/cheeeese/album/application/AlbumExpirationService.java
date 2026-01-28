80bd9fe1167f3cb44704d99797ee2e388413ab3a
다현 <123882512+dahyun24@users.noreply.github.com>
2026-01-28 21:29:21 +0900

    [CEEZ-72] fix: 불필요한 import문 제거

--- src/main/java/com/cheeeese/album/application/AlbumExpirationService.java
+++ src/main/java/com/cheeeese/album/application/AlbumExpirationService.java
@@ -19,7 +19,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
