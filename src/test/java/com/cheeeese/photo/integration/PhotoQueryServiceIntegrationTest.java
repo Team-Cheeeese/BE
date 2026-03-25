6550e10fdad6fe62a7ef8d1a62293aaeaa0d74f6
zyovn <yui2507@naver.com>
2026-03-26 00:01:12 +0900

    [CEEZ-77] feat: 사진 목록 조회 inclueMine 파라미터 제거

--- src/test/java/com/cheeeese/photo/integration/PhotoQueryServiceIntegrationTest.java
+++ src/test/java/com/cheeeese/photo/integration/PhotoQueryServiceIntegrationTest.java
@@ -83,7 +83,7 @@ public class PhotoQueryServiceIntegrationTest {
     void getPhotoList() {
         // when
         PhotoPageResponse page = photoQueryService.getPhotoPage(
-                testUser, testAlbum.getCode(), 0, 20, AlbumSorting.CREATED_AT, false
+                testUser, testAlbum.getCode(), 0, 20, AlbumSorting.CREATED_AT
         );
 
         // then
